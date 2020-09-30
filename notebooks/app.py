cf5fcbac8a41eb12feef092dc3bf974ce05bff2e
Rafael <rafaelmoreiracvo@yahoo.com.br>
2020-09-30 14:52:51 -0300

    Alterando base em 50%

---
+++ notebooks/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("Sistema Bom Pagador")
+st.markdown("Sistema de avaliador de clientes.")
+st.markdown("Clientes externos.")
+
+# Importando dataset externo que esta armazendo no drive.
+df_ext = pd.read_csv('/home/rafael/git/projeto_bom_pagador/dataset/banco_externo.csv')
+st.dataframe(df_ext.head(10))
+
+st.markdown("Clientes interno.")
+df_inter = pd.read_csv("/home/rafael/git/projeto_bom_pagador/dataset/banco_interno.csv")
+st.dataframe(df_inter.head(10))
