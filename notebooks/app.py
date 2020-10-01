312612f606c66041dc3b178774b9b971e5708fdf
Rafael <rafaelmoreiracvo@yahoo.com.br>
2020-09-30 22:15:58 -0300

    Unindo datasets

--- notebooks/app.py
+++ notebooks/app.py
@@ -10,11 +10,11 @@ st.markdown("Clientes externos.")
 df_ext = pd.read_csv('/home/rafael/git/projeto_bom_pagador/dataset/banco_externo.csv')
 st.dataframe(df_ext.head(10))
 
-#Teste
 st.markdown("Clientes interno.")
 df_inter = pd.read_csv("/home/rafael/git/projeto_bom_pagador/dataset/banco_interno.csv")
 st.dataframe(df_inter.head(10))
 
+# Unindo os datasets
 df = df_ext + df_inter
 
 x = df.drop('default', axis=1)
