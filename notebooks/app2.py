d07ad89d229f44371465b98be85c26b08de42f54
Rafael <rafaelmoreiracvo@yahoo.com.br>
2020-10-15 22:03:08 -0300

    consertando o modelo

--- notebooks/app2.py
+++ notebooks/app2.py
@@ -13,9 +13,6 @@ st.markdown("## Apresentação geral dos dados coletados.")
 
 df = pd.read_csv("/home/rafael/git/projeto_bom_pagador/notebooks/paulo/base2.csv", index_col='Unnamed: 0')
 
-transf = {0:"Aprovado", 1:"Reprovado"}
-df['Situação'] = df['Situação'].map(transf)
-
 st.dataframe(df)
 
 # Campos de entrada de dados do cliente.
@@ -66,26 +63,23 @@ tempo_empr = st.sidebar.number_input("Tempo de Empresa", value=0)
 
 taxa = st.sidebar.number_input("Taxa", value=0.)
 
-renda_empr = st.sidebar.number_input("Renda Emprestimo", value=0.)
-
 # Inserindo um botao na tela
 btn_predict = st.sidebar.button("Realizar consulta")
 
 #----------------- Classificação do cliente utilizando algoritmos ---------------------------------------#
 x = df.drop(['Situação'], axis=1)
 y = df['Situação']
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
-clf = GaussianNB()
-# clf = RandomForestClassifier(max_depth=15, random_state=0)
+# clf = GaussianNB()
+clf = RandomForestClassifier(max_depth=15)
 clf.fit(x_train, y_train)
 model_treinado = clf.predict(x_test)
 
-
 # Realizar a consulta quando o botao for acionado
 if btn_predict:
-    result = clf.predict([[renda_anual, idade, empr, finalidade, tempo_empr, taxa, renda_empr]])
+    result = clf.predict([[renda_anual, idade, empr, finalidade, tempo_empr, taxa]])
     sit = "Aprovado" if result[0] == 0 else "Reprovado"
     st.sidebar.write("O cliente foi ", sit)
-    acc = round(accuracy_score(y_test, model_treinado)*100,2)
+    acc = round(accuracy_score(y_test, model_treinado) * 100,2)
     st.sidebar.write("Accuracy do modelo é: ", acc)
